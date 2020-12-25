c75d6ea44d1ff00dee22201eb23195d0387ad974
熊致楷 <xiongzhikai@icloud.com>
2020-12-25 16:09:52 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
 
 export * from './src/main';
 export * from './src/type';
-export * from './src/cores/base-pipeline-node-module.core';
-export * from './src/cores/checker.core';
+export * from './src/cores/base-pipeline-module.core';
 export * from './src/modules/end/module';
 export * from './src/modules/modifier/module';
 export * from './src/modules/start/module';
